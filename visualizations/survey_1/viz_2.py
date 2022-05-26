e0dd9d2ee770bd967d440cd237c886910de4a53c
Pooja <59486401+bandpooja@users.noreply.github.com>
2022-05-26 00:48:52 -0400

    Add files via upload

--- visualizations/survey_1/viz_2.py
+++ visualizations/survey_1/viz_2.py
@@ -11,8 +11,10 @@ df.dropna(inplace=True)
 
 print(df)
 df_overall = df[df['Unnamed: 0'].str.contains('Overall')]
+df_ontario = df[df['Unnamed: 0'].str.contains('Ontario')]
 df_family = df[df['Unnamed: 0'].str.contains('arried')]
 print(df_overall)
+print(df_ontario)
 print(df_family)
 
 
@@ -23,13 +25,13 @@ per = df_overall.iloc[0].values.tolist()[1:]
 type_ = df_overall.iloc[0].values.tolist()[0]
 
 plt.subplot(1, 1, 1)
-ax, _, autotexts = plt.pie(x=[float(x) for x in per], startangle=90,
+ax1, _, autotexts = plt.pie(x=[float(x) for x in per], startangle=90,
                            autopct='%1.1f%%', textprops={'fontsize': 14}, radius=0.95)
 
 pattern = ['//', '..', 'xx']
 hatches = pattern
 
-for hatch, wedge in zip(hatches, ax):
+for hatch, wedge in zip(hatches, ax1):
     wedge.set_hatch(hatch)
     wedge.set_alpha(0.7)
     wedge.set_edgecolor('k')
@@ -37,8 +39,32 @@ for hatch, wedge in zip(hatches, ax):
 for text in autotexts:
     text.set_bbox(dict(facecolor='white', alpha=1.0, edgecolor='k'))
 
+labels_o = df_ontario.columns.values.tolist()[1:]
+per_o = df_ontario.iloc[0].values.tolist()[1:]
+type_o = df_ontario.iloc[0].values.tolist()[0]
+
+ax2, _, autotexts = plt.pie(x=[float(x) for x in per_o], startangle=90,
+                           autopct='%1.1f%%', textprops={'fontsize': 14}, radius=0.43)
+
+pattern = ['//', '..', 'xx']
+hatches = pattern
+
+for hatch, wedge in zip(hatches, ax2):
+    wedge.set_hatch(hatch)
+    wedge.set_alpha(0.9)
+    wedge.set_edgecolor('k')
+
+for text in autotexts:
+    text.set_bbox(dict(facecolor='white', alpha=1.0, edgecolor='k'))
+
+
 plt.title(f'Work efficiency {type_}', fontsize=20)
-labels = [f'{lbl.replace("/common-law", "").replace(" or common-law", "")} {str(p)} %' for lbl, p in zip(labels, per)]
-plt.legend(ax, labels, loc="upper right")
+labels = [f'{lbl.replace("/common-law", "").replace(" or common-law", "")} - ' + '$\\bf{Ontario}$' + f' {str(p)} %' for lbl, p in zip(labels_o, per_o)]
+labels += [f'{lbl.replace("/common-law", "").replace(" or common-law", "")} - ' + '$\\bf{Canada}$' + f' {str(p)} %' for lbl, p in zip(labels, per)]
+
+labels = labels[3:]
+plt.legend(ax2, labels, loc="upper right")
+# plt.legend(ax1, labels, loc="upper right")
+
 plt.tight_layout()
 plt.show()
